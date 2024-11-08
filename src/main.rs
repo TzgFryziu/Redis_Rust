838cd2d13036cb7ea8cd3810699d18acf4ed8559
tzgfryziu <patryk.fryzlewicz@gmail.com>
2024-11-08 17:43:10 +0100

    Handling concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,36 @@
 #![allow(unused_imports)]
 
-use std::io::{Read, Write};
-use std::net::TcpListener;
+use tokio::io;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
 
-fn main() {
+#[tokio::main]
+async fn main() -> io::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
 
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
-    for stream in listener.incoming() {
+    loop {
+        let stream = listener.accept().await;
+
         match stream {
-            Ok(mut stream) => {
-                loop {
+            Ok((mut stream, _)) => {
+                tokio::spawn(async move {
+                    println!("New connection");
                     let mut buffer = [0; 512];
-
-                    let read_count = stream.read(&mut buffer).unwrap();
-                    if read_count == 0 {
-                        break;
+                    loop {
+                        let read_count = stream.read(&mut buffer).await.unwrap();
+                        if read_count == 0 {
+                            println!("Connection ended");
+                            break;
+                        }
+                        stream.write(b"+PONG\r\n").await.unwrap();
                     }
-                    stream.write(b"+PONG\r\n").unwrap();
-                }
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
